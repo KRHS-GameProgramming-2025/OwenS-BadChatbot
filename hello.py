19b891246b459b9d3ae278f4350947e4d17e1f92
decafcatfeenavee <owen.salmon@kearsarge.org>
2024-09-26 09:21:21 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,6 @@
+name = input ("whawt iws ywour nwame X3")
+greeting = "haiiii"
+reminder = "don't forget the plus marks :3! or the parathesies!!!"
+remindertwo = "programming is cool!"
+print (greeting +  " " + name)
+print (reminder + " " + remindertwo)
